0668f7723ba9c4bbfa1ca62c205a979c9ddc691e
pranav-bolla <74841338+pranav-bolla@users.noreply.github.com>
2023-08-05 13:28:03 -0400

    1st

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+
+token = ''
+with open('token.txt') as f:
+    token = f.read()
+print(token)
+
+target_user_id = 269964731706507274
+
+intents = discord.Intents.default()
+intents.message_content = True  # To listen to message content
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.event
+async def on_message(message):
+    
+    if message.author.id == target_user_id:
+        if not message.author.bot:
+            await message.channel.send("who asked?")
+
+    await bot.process_commands(message)
+
+
+bot.run(token)
